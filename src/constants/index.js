49dfd59aac88fa71eb8e36ce35a1f5b9f7030519
5rahulmangla <5rahulmangla@gmail.com>
2025-08-05 19:43:31 +0530

    test commit

---
+++ src/constants/index.js
@@ -0,0 +1,296 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+  cts,
+  infy,
+  wipro,
+  atos,
+  capg
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'Microservices Architecture',
+    icon: ux,
+  },
+  {
+    title: 'API Integration',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'Rails',
+    icon: rubyrails,
+  },
+  {
+    name: 'graphql',
+    icon: graphql,
+  },
+  {
+    name: 'postgresql',
+    icon: postgresql,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Back-End Developer',
+    company_name: 'Cognizant Technology Solutions',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit deleniti, voluptas Lorem ipsum dolor sit amet consectetur adipisicing elit deleniti, voluptas Lorem ipsum dolor sit amet consectetur adipisicing elit deleniti, voluptas',
+    icon: cts,
+    iconBg: '#333333',
+    date: 'May 2014 - Sept 2017',
+  },
+  {
+    title: 'Back-End Developer',
+    company_name: 'Infosys Limited',
+     description: 'Capgemini Limited',
+    icon: infy,
+    iconBg: '#333333',
+    date: 'Sept 2017 - Sept 2019',
+  },
+  {
+    title: 'Full stack Developer',
+    company_name: 'Cognizant Technology Solutions',
+    description: 'Capgemini Limited',
+    icon: cts,
+    iconBg: '#333333',
+    date: 'Sept 2019 - April 2021',
+  },
+   {
+    title: 'Full stack Developer',
+    company_name: 'Atos Pvt Ltd',
+     description: 'Capgemini Limited',
+    icon: atos,
+    iconBg: '#333333',
+    date: 'April 2021 - Feb 2022',
+  },
+  {
+    title: 'Full stack Developer',
+    company_name: 'Wipro Limited',
+     description: 'Capgemini Limited',
+    icon: wipro,
+    iconBg: '#333333',
+    date: 'Feb 2022 - July 2025',
+  },
+    {
+    title: 'Full stack Developer',
+    company_name: 'Capgemini Limited',
+    description: 'Capgemini Limited',
+    icon: capg,
+    iconBg: '#333333',
+    date: 'July 2025 - Present',
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'KomiKult',
+    description: 'A comic characters list app that displays Marvel characters.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: komikult,
+    repo: 'https://github.com/shaqdeff/KomiKult',
+    demo: 'https://shaqdeff.github.io/KomiKult/',
+  },
+  {
+    id: 'project-2',
+    name: 'Leaderboard',
+    description:
+      'A leaderboard list app that displays scores submitted by different players.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: leaderboard,
+    repo: 'https://github.com/shaqdeff/Leaderboard',
+    demo: 'https://shaqdeff.github.io/Leaderboard/',
+  },
+  {
+    id: 'project-3',
+    name: 'Math Magicians',
+    description: 'This is a single-page calculator app built with React',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: math,
+    repo: 'https://github.com/shaqdeff/Math-Magicians',
+    demo: 'https://inspiring-medovik-37d3b3.netlify.app/',
+  },
+  {
+    id: 'project-4',
+    name: 'Movie Metro',
+    description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: movie,
+    repo: 'https://github.com/shaqdeff/Movie-Metro',
+    demo: 'https://movie-metro.netlify.app/',
+  },
+  {
+    id: 'project-5',
+    name: 'Nyeusi Fest Site',
+    description:
+      'This is a demo concert website for a music festival called Nyeusi.',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: nyeusi,
+    repo: 'https://github.com/shaqdeff/Nyeusi-Fest-Site',
+    demo: 'https://shaqdeff.github.io/Nyeusi-Fest-Site/',
+  },
+];
+
+export { services, technologies, experiences, projects };
